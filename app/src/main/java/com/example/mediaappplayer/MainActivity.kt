f5a2c2c4edd9438adbeb2ce46be3224a1f1629bf
Alex <hqwa.sm1@mail.ru>
2022-02-02 15:25:50 +0300

    Added recyclerView with swipeToDelete And Alert Dialog

--- app/src/main/java/com/example/mediaappplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaappplayer/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.mediaappplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mediaappplayer.adapters.MusicAdapter
+import com.example.mediaappplayer.databinding.ActivityMainBinding
+import com.example.mediaappplayer.dialogs.AddMusicDialog
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bind: ActivityMainBinding
+
+    private var adapter = MusicAdapter()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        bind.recyclerView.layoutManager = LinearLayoutManager(this)
+        bind.recyclerView.adapter = adapter
+
+
+        bind.btnAddMusic.setOnClickListener {
+            showMusicAddDialog()
+        }
+
+
+        var callback =
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean = false
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    (adapter).deleteMusic(viewHolder.adapterPosition)
+                }
+            }
+
+        var itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(bind.recyclerView)
+
     }
+
+    fun showMusicAddDialog() {
+        var musicAddDialog = AddMusicDialog()
+        musicAddDialog.show(supportFragmentManager, "musicAdd")
+        adapter.addMusic("Samuel Cargidres - Stunning Blade")
+
+    }
+
 }
